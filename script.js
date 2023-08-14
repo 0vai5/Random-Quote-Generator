ca21a667a94f3f589f7f400712d6ee2b143e4bec
Ovais Raza <129333571+0vai5@users.noreply.github.com>
2023-08-14 16:31:14 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,78 @@
+let randomNo =   Math.floor(Math.random() * 11);
+
+if (randomNo==0) {
+    
+    document.getElementById('img').src = "https://images.gr-assets.com/authors/1673611182p5/3565.jpg"
+    document.getElementById('p').innerHTML = '“Be yourself; everyone else is already taken.”';
+    document.getElementById('cite').innerHTML = '― Oscar Wilde';
+}
+else if(randomNo==1) {
+    document.getElementById('img').src = "https://images.gr-assets.com/quotes/1511992603p8/8630.jpg"
+    document.getElementById('p').innerHTML = '“I m selfish, impatient and a little insecure. I make mistakes, I am out of control and at times hard to handle. But if you can t handle me at my worst, then you sure as hell don t deserve me at my best.';
+    document.getElementById('cite').innerHTML = '― Marilyn Monroe';
+}
+else if (randomNo==2) {
+    document.getElementById('img').src = "https://images.gr-assets.com/authors/1429114964p5/9810.jpg"
+    document.getElementById('p').innerHTML = '“Two things are infinite: the universe and human stupidity; and I m not sure about the universe.”';
+    document.getElementById('cite').innerHTML = '― Albert Einstein';
+}
+else if (randomNo==3) {
+    document.getElementById('img').src = "https://images.gr-assets.com/authors/1315160559p5/22302.jpg"
+    document.getElementById('p').innerHTML = '“So many books, so little time.”';
+    document.getElementById('cite').innerHTML = '― Frank Zappa';
+}
+else if (randomNo==4) {
+    document.getElementById('img').src = "https://images.gr-assets.com/authors/1197881416p8/13755.jpg"
+    document.getElementById('p').innerHTML = '“A room without books is like a body without a soul.”';
+    document.getElementById('cite').innerHTML = '― Marcus Tullius Cicero';
+}
+else if (randomNo==5) {
+    document.getElementById('img').src = "https://images.gr-assets.com/authors/1198551937p5/259666.jpg"
+    document.getElementById('p').innerHTML = '“You only live once, but if you do it right, once is enough.”';
+    document.getElementById('cite').innerHTML = '― Mae West';
+}
+else if (randomNo==6) {
+    document.getElementById('img').src = "https://images.gr-assets.com/authors/1356810912p5/5810891.jpg"
+    document.getElementById('p').innerHTML = '“Be the change that you wish to see in the world.”';
+    document.getElementById('cite').innerHTML = '― Mahatma Gandhi';
+}
+else if (randomNo==7) {
+    document.getElementById('img').src = "https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/authors/1539614337i/63191.jpg"
+    document.getElementById('p').innerHTML = '<b>"فراد کے ہاتھوں میں ہے اقوام کی تقدیر <br> ہر فرد ہے ملّت کے مقدر کا ستارہ۔"</b>';
+    document.getElementById('cite').innerHTML = '― Allama Iqbal';
+}
+else if (randomNo==8) {
+    document.getElementById('img').src = "https://images.gr-assets.com/authors/1356810912p5/5810891.jpg"
+    document.getElementById('p').innerHTML = '“If you tell the truth, you don t have to remember anything.”';
+    document.getElementById('cite').innerHTML = '― Mark Twain';
+}
+else if (randomNo==9) {
+    document.getElementById('img').src = "https://images.gr-assets.com/authors/1216826209p5/114059.jpg"
+    document.getElementById('p').innerHTML = '“A friend is someone who knows all about you and still loves you.”';
+    document.getElementById('cite').innerHTML = '― Elbert Hubbard';
+}
+
+else if (randomNo==10) {
+    document.getElementById('img').src = "https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/authors/1370576671i/792644._UX200_CR0,29,200,200_.jpg"
+    document.getElementById('p').innerHTML = '"I do not believe in taking the right decision, I take a decision and make it right."';
+    document.getElementById('cite').innerHTML = '― Muhammad Ali Jinnah';
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+console.log(randomNo)
